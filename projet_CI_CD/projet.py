978f3d9b4cadcd391c506ec93a1f1c1064185af4
Mohamed EL AMRI <96185052+elamrimohamed1@users.noreply.github.com>
2023-01-31 23:00:20 +0100

    check syntax

--- projet_CI_CD/projet.py
+++ projet_CI_CD/projet.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from operator import attrgetter
+import sys 
 
 app = Flask(__name__)
 
@@ -74,3 +75,12 @@ def soldeUnClient(id):
     sortieEcran += str(personnes[int(id)].solde)
     return sortieEcran
 
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        if sys.argv[1] == "check_syntax":
+            print("Build [ OK ]")
+            exit(0)
+        else:
+            print("Passed argument not supported ! Supported argument : check_syntax")
+            exit(1)
+    app.run(debug=True)
