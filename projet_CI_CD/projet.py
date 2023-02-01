f19ec54bb67909866e38f18fc13cff98dfcbce66
omaramana <91990320+omaramana@users.noreply.github.com>
2023-02-01 23:00:38 +0100

    ajout du main
    ajout du main qui a été supprimé sans faire exprès

--- projet_CI_CD/projet.py
+++ projet_CI_CD/projet.py
@@ -105,3 +105,13 @@ def listerTransactionsCSV():
         print("Une erreur s'est produite.")      
     return afficherTransactions()
 
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        if sys.argv[1] == "check_syntax":
+            print("Build [ OK ]")
+            exit(0)
+        else:
+            print("Passed argument not supported ! Supported argument : check_syntax")
+            exit(1)
+    app.run(debug=True)
+
